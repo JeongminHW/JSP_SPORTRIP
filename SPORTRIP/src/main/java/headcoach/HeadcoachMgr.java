d192e8eab878019400dc3b296dc2ca541535a800
JUNG <20212856@office.deu.ac.kr>
2024-09-19 12:51:16 +0900

    add code
    add code

---
+++ SPORTRIP/src/main/java/headcoach/HeadcoachMgr.java
@@ -0,0 +1,42 @@
+package headcoach;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import DAO.DBConnectionMgr;
+
+public class HeadcoachMgr {
+	private DBConnectionMgr pool;
+	public HeadcoachMgr() {
+		pool = DBConnectionMgr.getInstance();
+	}
+	
+	// 팀 번호로 감독 검색
+	public HeadcoachBean getHeadcoach(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		HeadcoachBean bean = null;
+		try {
+			con = pool.getConnection();
+			sql = "select * from headcoach where team_num = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				bean = new HeadcoachBean();
+				bean.setHEADCOACH_NUM(rs.getInt(1));
+				bean.setHEADCOACH_NAME(rs.getString(2));
+				bean.setHEADCOACH_IMG(rs.getString(3));
+				bean.setTEAM_NUM(rs.getInt(4));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return bean;
+	}
+}
---
+++ SPORTRIP/src/main/java/stadium/StadiumMgr.java
@@ -0,0 +1,9 @@
+package stadium;
+
+public class StadiumMgr {
+	public StadiumMgr() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// 팀별 경기장 조회
+}
