608d4ef9e94957bf3c2907cd2ca094f3a04081f3
JUNG <20212856@office.deu.ac.kr>
2024-09-23 14:37:10 +0900

    feat : add BoardMgr
    feat : add BoardMgr

---
+++ SPORTRIP/src/main/java/board/BoardBean.java
@@ -0,0 +1,84 @@
+package board;
+
+public class BoardBean {
+	private int BOARD_NUM;
+	private String TITLE;
+	private String CONTENTS;
+	private String POSTDATE;
+	private String IP;
+	private String ID;
+	private int RECOMMAND;
+	private int NONRECOMMAND;
+	private int TEAM_NUM;
+	private int VIEWS;
+	private String BOARD_IMG;
+	
+	public int getBOARD_NUM() {
+		return BOARD_NUM;
+	}
+	public void setBOARD_NUM(int bOARD_NUM) {
+		BOARD_NUM = bOARD_NUM;
+	}
+	public String getTITLE() {
+		return TITLE;
+	}
+	public void setTITLE(String tITLE) {
+		TITLE = tITLE;
+	}
+	public String getCONTENTS() {
+		return CONTENTS;
+	}
+	public void setCONTENTS(String cONTENTS) {
+		CONTENTS = cONTENTS;
+	}
+	public String getPOSTDATE() {
+		return POSTDATE;
+	}
+	public void setPOSTDATE(String pOSTDATE) {
+		POSTDATE = pOSTDATE;
+	}
+	public String getIP() {
+		return IP;
+	}
+	public void setIP(String iP) {
+		IP = iP;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public int getRECOMMAND() {
+		return RECOMMAND;
+	}
+	public void setRECOMMAND(int rECOMMAND) {
+		RECOMMAND = rECOMMAND;
+	}
+	public int getNONRECOMMAND() {
+		return NONRECOMMAND;
+	}
+	public void setNONRECOMMAND(int nONRECOMMAND) {
+		NONRECOMMAND = nONRECOMMAND;
+	}
+	public int getTEAM_NUM() {
+		return TEAM_NUM;
+	}
+	public void setTEAM_NUM(int tEAM_NUM) {
+		TEAM_NUM = tEAM_NUM;
+	}
+	public int getVIEWS() {
+		return VIEWS;
+	}
+	public void setVIEWS(int vIEWS) {
+		VIEWS = vIEWS;
+	}
+	public String getBOARD_IMG() {
+		return BOARD_IMG;
+	}
+	public void setBOARD_IMG(String bOARD_IMG) {
+		BOARD_IMG = bOARD_IMG;
+	}
+	
+	
+}
