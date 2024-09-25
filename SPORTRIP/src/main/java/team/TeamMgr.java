1955486d5d8e7976e57ce03208355ff4c98c3e65
JUNG <20212856@office.deu.ac.kr>
2024-09-25 13:36:28 +0900

    update matchDate
    update matchDate

--- SPORTRIP/src/main/java/team/TeamMgr.java
+++ SPORTRIP/src/main/java/team/TeamMgr.java
@@ -3,17 +3,19 @@ package team;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 import DB.DBConnectionMgr;
 
 public class TeamMgr {
 	private DBConnectionMgr pool;
-	
+
 	public TeamMgr() {
 		pool = DBConnectionMgr.getInstance();
 	}
-	
+
 	// 선택된 스포츠 팀 리스트 출력
 	public Vector<TeamBean> listTeam(int sportNum) {
 		Connection con = null;
@@ -51,7 +53,7 @@ public class TeamMgr {
 		}
 		return vlist;
 	}
-	
+
 	// 선택된 하나의 팀 출력
 	public TeamBean getTeam(int teamNum) {
 		Connection con = null;
@@ -78,6 +80,7 @@ public class TeamMgr {
 				bean.setWIN_POINT(rs.getInt(10));
 				bean.setLOSS_POINT(rs.getInt(11));
 				bean.setCLUBINFO(rs.getString(12));
+				bean.setLOGO(rs.getString(13));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -86,7 +89,7 @@ public class TeamMgr {
 		}
 		return bean;
 	}
-	
+
 	// 팀 순위대로 출력
 	public Vector<TeamBean> TeamRank(int sportNum) {
 		Connection con = null;
@@ -123,5 +126,30 @@ public class TeamMgr {
 			pool.freeConnection(con, pstmt, rs);
 		}
 		return vlist;
+	}
+	
+	// 선택된 하나의 팀 일부 정보 출력
+	public String[] getTeamInfo(int teamNum) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		String[] info =  new String[2];;
+		try {
+			con = pool.getConnection();
+			sql = "select team_name, logo from team where team_num = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, teamNum);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				info[0] = (rs.getString(1));
+				info[1] = (rs.getString(2));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return info;
 	}
 }
