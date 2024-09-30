4e12eb623680d22c11130e150a8011242dba0f33
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2024-09-30 11:46:41 +0900

    updateSubHotel

--- SPORTRIP/src/main/java/room/RoomMgr.java
+++ SPORTRIP/src/main/java/room/RoomMgr.java
@@ -3,46 +3,49 @@ package room;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Vector;
-
+import java.util.ArrayList;
+import java.util.List;
 import DB.DBConnectionMgr;
 
 public class RoomMgr {
-	private DBConnectionMgr pool;
-	public RoomMgr() {
-		pool = DBConnectionMgr.getInstance();
-	}
-	
-	// 숙소번호, 객실번호에 따른 객실 출력
-	public Vector<RoomBean> getRoom(int lnum, int rnum) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = null;
-    	Vector<RoomBean> roomlist = new Vector<RoomBean>();
-		try {
-			con = pool.getConnection();
-			sql = "SELECT * FROM ROOM WHERE ROOM_NUM = ? AND LODGING_NUM = ?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, lnum);
-			pstmt.setInt(2, rnum);
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				RoomBean bean = new RoomBean();
-				bean.setROOM_NUM(rs.getInt(1));
-				bean.setLODGING_NUM(rs.getInt(2));
-				bean.setROOM_NAME(rs.getString(3));
-				bean.setSEAT_CAPACITY_R(rs.getInt(4));
-				bean.setROOM_PRICE(rs.getInt(5));
-				bean.setROOM_IMG(rs.getString(6));
-				bean.setRESERVE_NOW(rs.getInt(7));
-				roomlist.addElement(bean);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pool.freeConnection(con, pstmt, rs);
-		}
-		return roomlist;
-	}
+    private DBConnectionMgr pool;
+
+    public RoomMgr() {
+        pool = DBConnectionMgr.getInstance();
+    }
+
+    // 숙소번호에 따른 객실 출력
+    public List<RoomBean> getRoomsByLodgingNum(int lodgingNum) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = null;
+        List<RoomBean> roomlist = new ArrayList<>();
+
+        try {
+            con = pool.getConnection();
+            sql = "SELECT * FROM Room WHERE LODGING_NUM = ?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, lodgingNum);
+            rs = pstmt.executeQuery();
+            
+            while (rs.next()) {
+                RoomBean room = new RoomBean();
+                room.setROOM_NUM(rs.getInt("ROOM_NUM"));
+                room.setLODGING_NUM(rs.getInt("LODGING_NUM"));
+                room.setROOM_NAME(rs.getString("ROOM_NAME"));
+                room.setSEAT_CAPACITY_R(rs.getInt("SEAT_CAPACITY_R"));
+                room.setROOM_PRICE(rs.getInt("ROOM_PRICE"));
+                room.setROOM_IMG(rs.getString("ROOM_IMG"));
+                room.setRESERVE_NOW(rs.getInt("RESERVE_NOW"));
+                roomlist.add(room);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(con, pstmt, rs);
+        }
+        return roomlist;
+    }
+    
 }
